b7123031ff58c59e3e83b3c2a555de551864119e
Arash zandi <Arashz44@gmail.com>
2023-10-10 14:51:40 +0330

    add download through link

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DownloadEngine.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly IDownloadService _downloadService;
+    public Dictionary<string, string> fileNameUrlDownnloading = new();
+    public List<string> fileNameUrlDownnloaded = new();
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(IDownloadService downloadService)
     {
-        _logger = logger;
+        downloadService = _downloadService;
+        fileNameUrlDownnloading = Program.fileNameUrlDownnloading.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+        var files = new DirectoryInfo(Program.downloadRootPath).GetFiles();
+        foreach (var item in files)
+            fileNameUrlDownnloaded.Add(item.Name);
     }
+
+    public IWebHostEnvironment Env { get; }
 
     public void OnGet()
     {
-
     }
+    public async Task<IActionResult> OnPost()
+    {
+        var link = Request.Form["Link"].FirstOrDefault()?.ToString();
+        if (string.IsNullOrEmpty(link))
+            return Page();
+
+        var fileName = new Uri(link).Segments.Last();
+        Program.fileNameUrlDownnloading.Add(link, fileName);
+
+        if (!string.IsNullOrEmpty(link))
+            new TaskFactory().StartNew(async () =>
+            {
+                await _downloadService.DownloadFromUrlAsync(link, fileName);
+            });
+
+        return RedirectToPage("/index");
+    }
+
+    // [HttpGet("download")]
+    // public IActionResult GetBlobDownload([FromQuery] string link)
+    // {
+    //     var net = new System.Net.WebClient();
+    //     var data = net.DownloadData(link);
+    //     var content = new System.IO.MemoryStream(data);
+    //     var contentType = "APPLICATION/octet-stream";
+    //     var fileName = "something.bin";
+    //     return File(content, contentType, fileName);
+    // }
 }
--- Program.cs
+++ Program.cs
@@ -1,51 +1,69 @@
 using Microsoft.Extensions.FileProviders;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddDirectoryBrowser();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static IConfigurationRoot configurations;
+
+    public static string downloadFolder = "Downloadables";
+    public static string downloadRootPath = string.Empty;
+    // public static string downloadRootPath = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Downloadables")).Root;
+    public static Dictionary<string, string> fileNameUrlDownnloading = new();
+    private static void Main(string[] args)
+    {
+
+        configurations = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddDirectoryBrowser();
+        builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+        builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+        builder.Services.AddSingleton<IDownloadService, DownloadService>();
+        var app = builder.Build();
+
+        downloadRootPath = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, downloadFolder)).Root;
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseStaticFiles();
+        //app.UseStaticFiles(new StaticFileOptions
+        //{
+        //    FileProvider = new PhysicalFileProvider(
+        //           Path.Combine(builder.Environment.ContentRootPath, "Downloadables")),
+        //    RequestPath = "/Downloadables"
+        //});
+
+        var fileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Downloadables"));
+        var requestPath = "/Downloadables";
+
+        // Enable displaying browser links.
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = fileProvider,
+            RequestPath = requestPath
+        });
+
+        app.UseDirectoryBrowser(new DirectoryBrowserOptions
+        {
+            FileProvider = fileProvider,
+            RequestPath = requestPath
+        });
+
+
+        app.UseRouting();
+
+        app.MapRazorPages();
+
+        app.Run();
+    }
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-//app.UseStaticFiles(new StaticFileOptions
-//{
-//    FileProvider = new PhysicalFileProvider(
-//           Path.Combine(builder.Environment.ContentRootPath, "Downloadables")),
-//    RequestPath = "/Downloadables"
-//});
-
-var fileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Downloadables"));
-var requestPath = "/Downloadables";
-
-// Enable displaying browser links.
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = fileProvider,
-    RequestPath = requestPath
-});
-
-app.UseDirectoryBrowser(new DirectoryBrowserOptions
-{
-    FileProvider = fileProvider,
-    RequestPath = requestPath
-});
-
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
