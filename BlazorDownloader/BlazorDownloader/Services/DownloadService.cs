d94d54613966c024fb1bd311c35911b04210e65e
Arash zandi <Arashz44@gmail.com>
2023-10-11 13:03:59 +0330

    No Error

--- BlazorDownloader/BlazorDownloader/Services/DownloadService.cs
+++ BlazorDownloader/BlazorDownloader/Services/DownloadService.cs
@@ -13,12 +13,12 @@ public class Download
     public string url { get; set; }
     public string fileName { get; set; }
     public CancellationTokenSource cancellationTokenSource { get; set; }
-
+    public double? processPercent { get; set; }
 }
 
 public interface IDownloadService
 {
-    public Task<bool> DownloadFromUrlAsync(string downloadRootPath, string url, string fileName);
+    public Task<bool> DownloadFromUrlAsync(Func<bool> stateChange, string downloadRootPath, string url, string fileName);
 }
 public class DownloadService : IDownloadService
 {
@@ -33,7 +33,7 @@ public class DownloadService : IDownloadService
             fileNameUrlDownnloading.Remove(item);
         }
     }
-    public static void removeDownloadedItem(string fileName,string downloadRootPath)
+    public static void removeDownloadedItem(string fileName, string downloadRootPath)
     {
         // Clearing last downloaded fle
         fileNameUrlDownnloaded.Remove(fileName);
@@ -42,7 +42,7 @@ public class DownloadService : IDownloadService
             File.Delete(FileNamePath);
     }
 
-    public async Task<bool> DownloadFromUrlAsync(string downloadRootPath,string url, string fileName)
+    public async Task<bool> DownloadFromUrlAsync(Func<bool> stateChange, string downloadRootPath, string url, string fileName)
     {
         if (fileNameUrlDownnloading.Any(x => x.url == url)) { return false; }
         var thisDownload = new Download(url, fileName, new CancellationTokenSource());
@@ -53,28 +53,57 @@ public class DownloadService : IDownloadService
 
         removeDownloadedItem(fileName, fileNamePath);
 
+
         // Downloadning
+        //var fileInfo = new FileInfo(fileNamePath);
+        //try
+        //{
+        //    //HttpClient client = new HttpClient();
+        //    //using var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, thisDownload.cancellationTokenSource.Token);
+        //    //// You must use as stream to have control over buffering and number of bytes read/received
+        //    //using var stream = await response.Content.ReadAsStreamAsync();
+        //    //// Read/process bytes from stream as appropriate
+        //    //// Calculated by you based on how many bytes you have read.  Likely incremented within a loop.
+        //    //long bytesRecieved = 0;//...
+
+        //    //long? totalBytes = response.Content.Headers.ContentLength;
+        //    ////double? percentComplete = (double)bytesRecieved / totalBytes;
+        //    //thisDownload.processPercent = (double)bytesRecieved / totalBytes;
+        //    //// Do what you want with `percentComplete`
+        //    //stateChange();
+        //    //using var fileStream = fileInfo.OpenWrite();
+        //    //await stream.CopyToAsync(fileStream);
+        //}
+        //catch { }
+
+
+
+
+
         HttpResponseMessage response = null;
         try
         {
             response = await new HttpClient().GetAsync(url, thisDownload.cancellationTokenSource.Token);
         }
         catch { }
+
         if (response == null || !response.IsSuccessStatusCode)
         {
             removeDownloadingItem(url);
             return false;
         }
 
-        // Writing to file
+        //Writing to file
         var stream = await response.Content.ReadAsStreamAsync();
         var fileInfo = new FileInfo(fileNamePath);
         using var fileStream = fileInfo.OpenWrite();
         await stream.CopyToAsync(fileStream);
 
+
         fileNameUrlDownnloaded.Add(fileName);
         removeDownloadingItem(url);
 
+        stateChange();
         return true;
     }
 }
