7b2e84f86135388f3cd0ad35b4656522b8c57aeb
Arash zandi <Arashz44@gmail.com>
2023-10-11 22:32:11 +0330

    table

--- BlazorDownloader/BlazorDownloader/Services/DownloadService.cs
+++ BlazorDownloader/BlazorDownloader/Services/DownloadService.cs
@@ -15,6 +15,8 @@ public class Download
     public string fileName { get; set; }
     public CancellationTokenSource cancellationTokenSource { get; set; }
     public double? processPercent { get; set; }
+    public long downloadedSize { get; set; } = 0L;
+    public long? downloadFileSize { get; set; } = 0L;
 }
 
 public interface IDownloadService
@@ -67,12 +69,11 @@ public class DownloadService : IDownloadService
                 return false;
             }
 
-            var contentLength = response.Content.Headers.ContentLength;
+            thisDownload.downloadFileSize = response.Content.Headers.ContentLength;
 
             using var contentStream = await response.Content.ReadAsStreamAsync(thisDownload.cancellationTokenSource.Token);
             using var fileStream = new FileStream(DestinationFilePath, FileMode.Create, FileAccess.Write, FileShare.None, 8192, true);
 
-            var totalBytesRead = 0L;
             var buffer = new byte[8192];
             var isMoreToRead = true;
 
@@ -89,9 +90,8 @@ public class DownloadService : IDownloadService
 
                 await fileStream.WriteAsync(buffer, 0, bytesRead, thisDownload.cancellationTokenSource.Token);
 
-                totalBytesRead += bytesRead;
-                thisDownload.processPercent = totalBytesRead * 1d / contentLength.Value * 100;
-                //Console.WriteLine($"Downloaded {totalBytesRead} of {contentLength} bytes. {thisDownload.processPercent:0.##}% complete.");
+                thisDownload.downloadedSize += bytesRead;
+                thisDownload.processPercent = thisDownload.downloadedSize * 1d / (thisDownload.downloadFileSize ?? 0) * 100;
                 stateChange();
             } while (isMoreToRead && !thisDownload.cancellationTokenSource.IsCancellationRequested);
 
@@ -106,49 +106,3 @@ public class DownloadService : IDownloadService
         return true;
     }
 }
-
-
-
-//HttpResponseMessage response = null;
-//try
-//{
-//    response = await new HttpClient().GetAsync(url, thisDownload.cancellationTokenSource.Token);
-//}
-//catch { }
-
-//if (response == null || !response.IsSuccessStatusCode)
-//{
-//    removeDownloadingItem(url);
-//    return false;
-//}
-
-////Writing to file
-//var stream = await response.Content.ReadAsStreamAsync();
-//var fileInfo = new FileInfo(fileNamePath);
-//using var fileStream = fileInfo.OpenWrite();
-//await stream.CopyToAsync(fileStream);
-
-
-
-
-// Downloadning
-//var fileInfo = new FileInfo(fileNamePath);
-//try
-//{
-//    //HttpClient client = new HttpClient();
-//    //using var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, thisDownload.cancellationTokenSource.Token);
-//    //// You must use as stream to have control over buffering and number of bytes read/received
-//    //using var stream = await response.Content.ReadAsStreamAsync();
-//    //// Read/process bytes from stream as appropriate
-//    //// Calculated by you based on how many bytes you have read.  Likely incremented within a loop.
-//    //long bytesRecieved = 0;//...
-
-//    //long? totalBytes = response.Content.Headers.ContentLength;
-//    ////double? percentComplete = (double)bytesRecieved / totalBytes;
-//    //thisDownload.processPercent = (double)bytesRecieved / totalBytes;
-//    //// Do what you want with `percentComplete`
-//    //stateChange();
-//    //using var fileStream = fileInfo.OpenWrite();
-//    //await stream.CopyToAsync(fileStream);
-//}
-//catch { }
